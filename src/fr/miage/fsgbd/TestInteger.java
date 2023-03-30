8c012cbcb239e14c1158d54858dae6afa409cb70
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-03-30 14:33:03 +0200

    Amélioration de la mise en forme des fichiers de test

--- src/fr/miage/fsgbd/TestInteger.java
+++ src/fr/miage/fsgbd/TestInteger.java
@@ -1,7 +1,9 @@
 package fr.miage.fsgbd;
 
-public class TestInteger implements Executable<Integer>, java.io.Serializable {
-    public boolean execute(Integer int1, Integer int2) {
-        return (int1 < int2);
-    }
+public class TestInteger implements Executable<Integer>
+{
+	public boolean execute(Integer int1, Integer int2)
+	{
+		return (int1 < int2);
+	}
 }
